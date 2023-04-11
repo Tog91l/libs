c3a0c2e8d5b4877fa97fe26fd79d5b6285c412f1
Tog91l <114440739+Tog91l@users.noreply.github.com>
2023-04-11 22:43:32 +0300

    Update matrixs.hpp

--- glushko_t_a/prj.lab/matrixs/include/matrixs/matrixs.hpp
+++ glushko_t_a/prj.lab/matrixs/include/matrixs/matrixs.hpp
@@ -1,1 +1,29 @@
+#pragma once
+#include <iostream>
+#include <string>
+#ifndef MATRIXS_MATRIXS_HPP_20230215
+#define MATRIXS_MATRIXS_HPP_20230215
 
+class MatrixS {
+
+public:
+	using SizeType = std::tuple<std::ptrdiff_t, std::ptrdiff_t>;
+
+public:
+	explicit MatrixS(const SizeType& size = { 0,0 });
+	MatrixS(const std::ptrdiff_t rows, const std::ptrdiff_t colns);
+	~MatrixS();
+
+	MatrixS(const MatrixS& other);
+	MatrixS& operator=(const MatrixS& other);
+
+	void resize(const SizeType& new_size);
+	void resize(const std::ptrdiff_t i, const std::ptrdiff_t j);
+
+private:
+	ptrdiff_t _rows;
+	ptrdiff_t _colns;
+	int* data;
+};
+
+#endif
