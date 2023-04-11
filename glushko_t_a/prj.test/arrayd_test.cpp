f218bcf3bf6712a3536ea5a0e70357da7a03193a
Tog91l <114440739+Tog91l@users.noreply.github.com>
2023-04-11 21:50:11 +0300

    Update arrayd_test.cpp

--- glushko_t_a/prj.test/arrayd_test.cpp
+++ glushko_t_a/prj.test/arrayd_test.cpp
@@ -1,1 +1,10 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
 
+#include <arrayd/arrayd.hpp>
+
+TEST_CASE("[arrayd] - ArrayD bool operators") {
+	ArrayD a(5,3);
+	ArrayD b(2,3);
+
+}
