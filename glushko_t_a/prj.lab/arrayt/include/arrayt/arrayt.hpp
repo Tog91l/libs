f07b116b8d462222961271ecbd5555733a1f0074
Tog91l <114440739+Tog91l@users.noreply.github.com>
2023-04-11 22:19:12 +0300

    Update arrayt.hpp

--- glushko_t_a/prj.lab/arrayt/include/arrayt/arrayt.hpp
+++ glushko_t_a/prj.lab/arrayt/include/arrayt/arrayt.hpp
@@ -1,1 +1,44 @@
+#pragma once
+#include <iostream>
+#include <string>
+#ifndef ARRAYT_ARRAYT_HPP_20230215
+#define ARRAYT_ARRAYT_HPP_20230215
 
+template<typename T>
+class ArrayT {
+public:
+	ArrayT();
+	ArrayT( const ptrdiff_t length);
+	ArrayT(const ArrayT&);
+	ArrayT& operator=(const ArrayT&);
+	~ArrayT();
+
+	
+
+	std::ptrdiff_t ssize() noexcept;
+
+	T& operator[](const ptrdiff_t i);
+	const T& operator[](const ptrdiff_t i)const; // i - index
+
+	
+	void resize( const ptrdiff_t new_size);
+	void insert(const ptrdiff_t i, const double num);
+	void remove(const ptrdiff_t i);
+
+	
+
+	std::ostream writeTo(std::ostream& ostrm)const;
+	//std::istream readFrom(std::istream& istrm);
+
+private:
+	std:: ptrdiff_t size;
+    T* memory;
+
+
+};
+
+
+
+
+
+#endif
