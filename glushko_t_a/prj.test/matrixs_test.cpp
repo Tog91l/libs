25a5200738cc8e559799efb1ad077c16122e76ec
Tog91l <114440739+Tog91l@users.noreply.github.com>
2023-04-11 22:46:14 +0300

    Update matrixs_test.cpp

--- glushko_t_a/prj.test/matrixs_test.cpp
+++ glushko_t_a/prj.test/matrixs_test.cpp
@@ -1,1 +1,10 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
 
+#include <matrixs/matrixs.hpp>
+
+TEST_CASE("[matrixs] - MatrixS ctor") {
+	MatrixS a(4, 4);
+	MatrixS b(4, 6);
+	
+}
