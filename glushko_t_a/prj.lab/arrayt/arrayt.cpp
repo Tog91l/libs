b06495530af84733e13d233c0320041a0c5e1c82
Tog91l <114440739+Tog91l@users.noreply.github.com>
2023-04-11 22:20:34 +0300

    Update arrayt.cpp

--- glushko_t_a/prj.lab/arrayt/arrayt.cpp
+++ glushko_t_a/prj.lab/arrayt/arrayt.cpp
@@ -1,1 +1,62 @@
+#include <arrayt/arrayt.hpp>
+
+template<typename T>
+ArrayT<T>::ArrayT() {
+	memory = nullptr;
+	size = 0;
+	
+}
+
+template<typename T>
+ArrayT<T>::~ArrayT() {
+	delete[] memory;
+	return;
+}
+template<typename T>
+void ArrayT<T>::resize(ptrdiff_t new_size) {
+	if (new_size < 0) {
+		throw std::invalid_argument("ArrayD can't be less than zero");
+	}
+	if (new_size > 0) {
+
+		double* memory_temp;
+		memory_temp = new double[new_size];
+		for (int i = 0; i < size; i++) {
+			memory_temp[i] = memory[i];
+		}
+		for (int i = size; i < new_size; i++) {
+			memory_temp[i] = 0;
+		}
+		delete[] memory;
+		memory = new double[size];
+		for (int i = 0; i < size; i++) {
+			memory[i] = memory_temp[i];
+		}
+		delete[] memory_temp;
+	}
+}
+
+	/*void ArrayD::insert(const std::ptrdiff_t i, const double num)
+	{
+		if (i<0 || i >size()) {
+			throw std::invalid_argument("you don't need to use it");
+		}
+		resize(size() + 1);
+		for (int l = i + 1; i < size(); l++) {
+			memory[l] = memory[l - 1];
+		}
+		memory[i] = num;
+	}*/
+     
+template<typename T>
+	void ArrayT<T>::remove(const std::ptrdiff_t i) {
+		if (i < 0 || i > size()) {
+			throw std::invalid_argument("you don't need to use it");
+		}
+		resize(size() - 1);
+		for (int l = i + 1; i < size(); l++) {
+			memory[l] = memory[l - 1];
+		}
+		return;
+	}
 
