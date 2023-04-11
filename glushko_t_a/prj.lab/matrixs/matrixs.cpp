a12fc47dfafd09d854157d746d9d9692ff67a01c
Tog91l <114440739+Tog91l@users.noreply.github.com>
2023-04-11 22:45:20 +0300

    Update matrixs.cpp

--- glushko_t_a/prj.lab/matrixs/matrixs.cpp
+++ glushko_t_a/prj.lab/matrixs/matrixs.cpp
@@ -1,1 +1,32 @@
+#include <matrixs/matrixs.hpp>
 
+MatrixS::MatrixS(const SizeType& size) {
+	if (_rows < 0 || _colns < 0) {
+		throw std::invalid_argument("Size can't be negative");
+	}
+}
+
+MatrixS::~MatrixS() {
+	delete[] data;
+}
+
+void  MatrixS::resize(const std::ptrdiff_t row, const std::ptrdiff_t coln) {
+	if (row <= 0 || coln <= 0) {
+		throw std::invalid_argument("Sizes must be positive");
+	}
+	int* data_temp;
+	data_temp = new int[row, coln];
+	for (int i = 0; i < row; i++) {
+		for (int c = 0; c < coln; c++) {
+
+		}
+	}
+	delete[] data_temp;
+}
+
+void MatrixS::resize(const SizeType& new_size) {
+	//if (new_size <= 0) {
+	//	throw std::invalid_argument("Sizes must be positive");
+	//}
+
+}
