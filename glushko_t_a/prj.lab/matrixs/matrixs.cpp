fe46049a67350d2d4d9ae4276b9d43eea4725360
Tog91l <114440739+Tog91l@users.noreply.github.com>
2023-05-04 22:19:49 +0300

    Update matrixs.cpp

--- glushko_t_a/prj.lab/matrixs/matrixs.cpp
+++ glushko_t_a/prj.lab/matrixs/matrixs.cpp
@@ -1,13 +1,54 @@
 #include <matrixs/matrixs.hpp>
 
-MatrixS::MatrixS(const SizeType& size) {
-	if (_rows < 0 || _colns < 0) {
+MatrixS::MatrixS() {
+	rows = 0;
+	cols = 0;
+	data = nullptr;
+}
+
+
+MatrixS::MatrixS(const SizeType& s) {
+	if (rows < 0 || cols < 0) {
 		throw std::invalid_argument("Size can't be negative");
 	}
 }
 
+MatrixS::MatrixS(std::ptrdiff_t rows, std::ptrdiff_t cols) {
+
+}
+
+
+MatrixS::MatrixS(std::ptrdiff_t rows, std::ptrdiff_t cols, int num) {
+
+}
+
+const int& MatrixS::at(const std::ptrdiff_t r, const std::ptrdiff_t c)const {
+	return data[r, c];
+}
+
+int& MatrixS::at(const std::ptrdiff_t r, const std::ptrdiff_t c) {
+	return data[r, c];
+}
+
+const int& MatrixS::at(const MatrixS::SizeType s)const {
+	return data[0];
+}
+
+int& MatrixS::at(const MatrixS::SizeType s) {
+	return data[0];
+}
+
 MatrixS::~MatrixS() {
 	delete[] data;
+}
+
+
+std::ptrdiff_t MatrixS::NRows()const noexcept {
+	return rows;
+}
+
+std::ptrdiff_t MatrixS::NCols()const noexcept {
+	return cols;
 }
 
 void  MatrixS::resize(const std::ptrdiff_t row, const std::ptrdiff_t coln) {
@@ -24,9 +65,13 @@ void  MatrixS::resize(const std::ptrdiff_t row, const std::ptrdiff_t coln) {
 	delete[] data_temp;
 }
 
-void MatrixS::resize(const SizeType& new_size) {
+void MatrixS::resize(const SizeType& s) {
 	//if (new_size <= 0) {
 	//	throw std::invalid_argument("Sizes must be positive");
 	//}
 
+}
+
+MatrixS::SizeType MatrixS::ssize()const noexcept {
+	return SizeType(rows, cols);
 }
