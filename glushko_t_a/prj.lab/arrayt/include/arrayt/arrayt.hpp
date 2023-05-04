498184966c3ef191807eaf6b6bcc402b99cd7710
Tog91l <114440739+Tog91l@users.noreply.github.com>
2023-05-04 22:18:12 +0300

    Update arrayt.hpp

--- glushko_t_a/prj.lab/arrayt/include/arrayt/arrayt.hpp
+++ glushko_t_a/prj.lab/arrayt/include/arrayt/arrayt.hpp
@@ -8,37 +8,31 @@ template<typename T>
 class ArrayT {
 public:
 	ArrayT();
-	ArrayT( const ptrdiff_t length);
-	ArrayT(const ArrayT&);
-	ArrayT& operator=(const ArrayT&);
+	explicit ArrayT( const std:: ptrdiff_t length);
+	ArrayT(const ArrayT<T>&);
+	ArrayT(std::ptrdiff_t size_c, T num);
 	~ArrayT();
 
 	
 
-	std::ptrdiff_t ssize() noexcept;
+	std::ptrdiff_t ssize() const;
 
-	T& operator[](const ptrdiff_t i);
-	const T& operator[](const ptrdiff_t i)const; // i - index
+	T& operator[](std:: ptrdiff_t i);
+	const T& operator[](std:: ptrdiff_t i)const; // i - index
 
 	
-	void resize( const ptrdiff_t new_size);
-	void insert(const ptrdiff_t i, const double num);
-	void remove(const ptrdiff_t i);
+	void resize( std:: ptrdiff_t new_size);
+	void insert(std::ptrdiff_t, T num);
+	void remove(std:: ptrdiff_t);
 
 	
 
 	std::ostream writeTo(std::ostream& ostrm)const;
-	//std::istream readFrom(std::istream& istrm);
+	
 
 private:
 	std:: ptrdiff_t size;
     T* memory;
 
-
+	static const char comm = ',';
 };
-
-
-
-
-
-#endif
